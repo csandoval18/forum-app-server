13ade6dae62be9d03f952c877c6495aa15f73b33
Christian AS <alvitesscf18@uww.edu>
2022-06-24 03:01:37 -0500

    connection to db

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log('hello world')
+import { MikroORM } from '@mikro-orm/core'
+
+const main = async () => {
+	const orm = await MikroORM.init({
+		dbName: 'forum',
+		user: 'Christian',
+		password: 'root',
+		type: 'postgresql',
+		debug: process.env.NODE_ENV !== 'production',
+	})
+}
+
+console.log('hello there')
