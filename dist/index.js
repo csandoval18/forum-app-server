ac3614a77a865a619d5a1cef9c32b47470d16d09
csandoval18 <christian_alvites@outlook.com>
2022-06-27 02:37:12 -0500

    started mikroorm setup

--- dist/index.js
+++ dist/index.js
@@ -8,16 +8,23 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@mikro-orm/core");
+const Post_1 = require("./entities/Post");
+const mikro_orm_config_1 = __importDefault(require("./mikro-orm.config"));
 const main = () => __awaiter(void 0, void 0, void 0, function* () {
-    const orm = yield core_1.MikroORM.init({
-        dbName: 'forum',
-        user: 'Christian',
-        password: 'root',
-        type: 'postgresql',
-        debug: process.env.NODE_ENV !== 'production',
+    const orm = yield core_1.MikroORM.init(mikro_orm_config_1.default);
+    const post = orm.em.create(Post_1.Post, {
+        title: 'my first post',
     });
+    yield orm.em.persistAndFlush(post);
+    console.log('-----------sql 2------------');
+    yield orm.em.nativeInsert(Post_1.Post, { title: 'my first post 2' });
 });
-console.log('hello there');
+main().catch((err) => {
+    console.error(err);
+});
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,20 @@
-import { MikroORM } from '@mikro-orm/core'
+import { MikroORM, RequiredEntityData } from '@mikro-orm/core'
+import { __prod__ } from './constants'
+import { Post } from './entities/Post'
+import microConfig from './mikro-orm.config'
 
 const main = async () => {
-	const orm = await MikroORM.init({
-		dbName: 'forum',
-		user: 'Christian',
-		password: 'root',
-		type: 'postgresql',
-		debug: process.env.NODE_ENV !== 'production',
-	})
+	const orm = await MikroORM.init(microConfig)
+
+	//meta.object.member.ts
+	const post = orm.em.create(Post, {
+		title: 'my first post',
+	} as RequiredEntityData<Post>)
+	await orm.em.persistAndFlush(post)
+	console.log('-----------sql 2------------')
+	await orm.em.nativeInsert(Post, { title: 'my first post 2' })
 }
 
-console.log('hello there')
+main().catch((err) => {
+	console.error(err)
+})
